03f737d2320cd05a5abb88622038cccc3bd09be3
Laptev Mikhail <bugi-wugi93@mail.ru>
2018-03-28 21:50:00 +0300

    added chart

--- AspNetMVCTestTask/Controllers/HomeController.cs
+++ AspNetMVCTestTask/Controllers/HomeController.cs
@@ -1,9 +1,12 @@
 ﻿using AspNetMVCTestTask.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Helpers;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace AspNetMVCTestTask.Controllers
 {
@@ -23,7 +26,7 @@ namespace AspNetMVCTestTask.Controllers
         }
 
 
-       
+
         [HttpPost]
         public ActionResult Index(StockExchangeData data)
         {
@@ -32,5 +35,58 @@ namespace AspNetMVCTestTask.Controllers
             db.SaveChanges();
             return Index();
         }
+
+        [HttpGet]
+        public ActionResult MakeChart()
+        {
+
+            IEnumerable<StockExchangeData> data = db.StockExchangeData;
+
+            var myChart = new Chart(width: 600, height: 400)
+                .AddTitle("Chart Title")
+                .AddSeries(
+                    name: "Profit",
+                    xValue: data.Select(d => d.BusinessDay).ToArray(),
+                    yValues: data.Select(d => d.Profit).ToArray());
+
+            ViewBag.Chart = myChart;
+
+
+            // возвращаем представление
+            return View();
+        }
+
+
+        [HttpGet]
+        public FileResult Download()
+        {
+            string path = Server.MapPath("~/App_Data/Files/excelFile.xlsx");
+
+            Excel.Application excelApp = new Excel.Application { Visible = false };
+            var xlWorkBook = excelApp.Workbooks.Add();
+
+            var xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.Item[1];
+            xlWorkSheet.Range["A1", "B1"].Value2 = "Показатели Заемщика";
+            xlWorkSheet.Cells[1, 3] = "Валюта";
+            xlWorkSheet.Cells[1, 4] = "Индексы";
+            xlWorkSheet.Cells[2, 1] = "Дата";
+            xlWorkSheet.Cells[2, 2] = "Выручка";
+            xlWorkSheet.Cells[2, 3] = "серебро, руб. ";
+            xlWorkSheet.Cells[2, 4] = "Индекс ММВБ Last";
+            xlWorkBook.SaveAs(path, Excel.XlFileFormat.xlWorkbookNormal);
+            xlWorkBook.Close(true);
+            excelApp.Quit();
+
+
+            
+            // Объект Stream
+            FileStream fs = new FileStream(path, FileMode.Open);
+            string file_type = "application/excel";
+            string file_name = "excelFile.xls";
+            return File(fs, file_type, file_name);
+
+        }
+
+
     }
 }
--- AspNetMVCTestTask/Models/StockExchangeDataInitializer.cs
+++ AspNetMVCTestTask/Models/StockExchangeDataInitializer.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data.Entity;
-using System.IO;
-using System.Linq;
-using System.Reflection;
 using System.Web;
 using Excel = Microsoft.Office.Interop.Excel;
 
